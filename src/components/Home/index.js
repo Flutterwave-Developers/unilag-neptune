b93fba98eebad76eb87017dcfc4cb5ac07fdb334
Akin Aguda <lordtitan7@gmail.com>
2019-10-05 15:15:06 +0100

    done again

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -15,7 +15,31 @@ const Home = () => {
                 <img src="/back.png" alt="back" className={Styles.mainImg}/>
             </div>
             <p className={Styles.dec}> Edukos is a platform that allows students and teachers to connect from around the world, schedule one on one classes and teaches or learn directly, one student to a teacher</p>
-            {/* <hr className={Styles.line}/> */}
+            <hr className={Styles.line}/>
+            <div className={Styles.section}>
+                <img src="/teacher.png" alt="g"/>
+                <div className={Styles.caption}>
+                    <h2>Access 1000+ Courses and Ready Tutors</h2>
+                    <p>With Edukos, a student has access to as many as 1000+ teachers who are proficient in even more courses</p>
+                    <button>Get Started</button>
+                </div>
+            </div>
+            <div className={Styles.section}>
+                <div className={Styles.caption}>
+                    <h2>Schedule One On One Classes</h2>
+                    <p>With Edukos, a student can schedule one on one class sessions with teachers to take them personally on a particular course or join others on their schedule for the same teacher and course.</p>
+                    <button>Browse teachers</button>
+                </div>
+                <img src="/grp.png" alt="g"/>
+            </div>
+            <div className={Styles.section}>
+                <img src="/sphere.png" alt="g"/>
+                <div className={Styles.caption}>
+                    <h2>Pay For Services With Rave by FlutterWave</h2>
+                    <p>With Edukos, students pay for our service and to the teachers. Payment is very secure and reliable as we use Rave by Flutterwave. teachers also get paid using Rave</p>
+                    <button>Browse teachers</button>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/elements/ProfCard/index.js
+++ src/components/elements/ProfCard/index.js
@@ -4,7 +4,14 @@ import Styles from './index.module.css';
 const ProfCard= () => {
     return (
         <div className={Styles.container}>
-            
+            <div className={Styles.f}>
+                <span className={Styles.a}>Time Left</span>
+                <span className={Styles.num}>25mins</span> 
+            </div>
+                <div>
+                <span className={Styles.a}>Time Spent</span>
+                <span className={Styles.num}>1 hr 30 mins</span>
+                </div>
         </div>
     )
 }
